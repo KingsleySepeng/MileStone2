656431c0dbdd9636f912d69c9cd1d02f798aef89
KingsleySepeng <91886868+KingsleySepeng@users.noreply.github.com>
2021-10-13 21:53:09 +0200

    Add project files.

---
+++ DataHandler.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+namespace MileStone2
+{
+    class DataHandler
+    {
+        //  DataHandler() { }
+        string con = "Data Source=DESKTOP-C2RP5S6;Initial Catalog=Project_DB;Integrated Security=True";
+        //
+        public DataTable SearchStudent(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(con))
+            {
+                string SearchQuery = "SELECT * FROM Student WHERE StudentID =" + id;
+                SqlCommand cmd = new SqlCommand(SearchQuery, connection);
+                cmd.Parameters.AddWithValue("StudentID", id);
+
+                DataTable dt = new DataTable();
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    dt.Load(dr);
+                    return dt;
+                }
+                connection.Close();
+            }
+
+
+
+        }
+        public DataTable SearchModule(int code)
+        {
+            using (SqlConnection connection = new SqlConnection(con))
+            {
+                string SearchQuery = "SELECT * FROM Module WHERE ModuleCode =" + code;
+                SqlCommand cmd = new SqlCommand(SearchQuery, connection);
+                cmd.Parameters.AddWithValue("ModuelCode", code);
+                connection.Open();
+                DataTable dt = new DataTable();
+                using (SqlDataReader dr = cmd.ExecuteReader())
+                {
+                    dt.Load(dr);
+                    return dt;
+                }
+                connection.Close();
+            }
+
+        }
+
+        //===========================================================================================
+
+        public void CreateStudent(int id, string name, string surname, DateTime DOB, string gender, string phone, string address, string module)
+        { //choose image option
+            using (SqlConnection connection = new SqlConnection(con))
+            {
+                string CreateQuery1 = "INSERT INTO Student(StudentID,FirstName,LastName,DOB,Gender,Phone,Address,ModuleCode)  VALUES(" + id + name + surname + DOB + gender + phone + address + module + ")";
+                SqlCommand cmd1 = new SqlCommand(CreateQuery1, connection);
+                cmd1.Parameters.AddWithValue("StudentID", id);
+                cmd1.Parameters.AddWithValue("FirstName", name);
+                cmd1.Parameters.AddWithValue("LastName", surname);
+                cmd1.Parameters.AddWithValue("DOB", DOB);
+                cmd1.Parameters.AddWithValue("Gender", gender);
+                cmd1.Parameters.AddWithValue("Phone", phone);
+                cmd1.Parameters.AddWithValue("Address", address);
+                cmd1.Parameters.AddWithValue("ModuleCode", module);
+                connection.Open();
+                cmd1.ExecuteNonQuery();
+                connection.Close();
+            }
+
+        }
+
+        public void CreateModule(string module, string ModuleName, string Description, string link)
+        { //choose image option
+            using (SqlConnection connection = new SqlConnection(con))
+            {
+                string CreateQuery2 = "INSERT INTO Module(ModuleCode,Name,Description,Link)  VALUES(" + module + ModuleName + Description + link + ")";
+                SqlCommand cmd1 = new SqlCommand(CreateQuery2, connection);
+                cmd1.Parameters.AddWithValue("moduleCode", module);
+                cmd1.Parameters.AddWithValue("Name", ModuleName);
+                cmd1.Parameters.AddWithValue("Description", Description);
+                cmd1.Parameters.AddWithValue("Link", link);
+                cmd1.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+
+        //=================================================================================================
+        public DataTable ReadStudent()
+        {
+            //use datatable
+            SqlConnection connection = new SqlConnection(con);
+            connection.Open();
+            string ReadQuery = "SELECT * FROM Student";
+            SqlDataAdapter cmd = new SqlDataAdapter(ReadQuery, connection);
+            // SqlCommand cmd = new SqlCommand(ReadQuery,connection);
+
+            DataTable dt = new DataTable();
+            cmd.Fill(dt);
+            return dt;
+
+
+
+
+        }
+        public DataTable ReadModule()
+        {
+            //use datatable
+            SqlConnection connection = new SqlConnection(con);
+            connection.Open();
+            string ReadQuery = "SELECT * FROM Module";
+            SqlDataAdapter cmd = new SqlDataAdapter(ReadQuery, connection);
+            // SqlCommand cmd = new SqlCommand(ReadQuery,connection);
+
+            DataTable dt = new DataTable();
+            cmd.Fill(dt);
+            return dt;
+
+        }
+
+        //=======================================================================================================
+        public void UpdateStudent(int id, string name, string surname, DateTime DOB, string gender, string phone, string address, string module)
+        {
+            SqlConnection connection = new SqlConnection(con);
+            connection.Open();
+            string UpdateQuery1 = "UPDATE student SET FirstName =, LastName =, DOB = ,Gender =,Phone = , Address = , ModuleCode = ,  WHERE StudentID = ";
+            SqlCommand cmd1 = new SqlCommand(UpdateQuery1, connection);
+            cmd1.Parameters.AddWithValue("StudentID", id);
+            cmd1.Parameters.AddWithValue("FirstName", name);
+            cmd1.Parameters.AddWithValue("LastName", surname);
+            cmd1.Parameters.AddWithValue("DOB", DOB);
+            cmd1.Parameters.AddWithValue("Gender", gender);
+            cmd1.Parameters.AddWithValue("Phone", phone);
+            cmd1.Parameters.AddWithValue("Address", address);
+            cmd1.Parameters.AddWithValue("ModuleCode", module);
+            cmd1.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        public void UpdateModule(string module, string ModuleName, string Description, string link)
+        {
+            SqlConnection connection = new SqlConnection(con);
+            connection.Open();
+
+            string UpdateQuery2 = "UPDATE Module SET ModuleCode=, Name = , Description = , Link =   WHERE ModuleCode = ";
+            SqlCommand cmd1 = new SqlCommand(UpdateQuery2, connection);
+            cmd1.Parameters.AddWithValue("moduleCode", module);
+            cmd1.Parameters.AddWithValue("Name", ModuleName);
+            cmd1.Parameters.AddWithValue("Description", Description);
+            cmd1.Parameters.AddWithValue("Link", link);
+            cmd1.ExecuteNonQuery();
+            connection.Close();
+        }
+        //=========================================================================================================
+        public void DeleteStudent(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(con))
+            {
+                string DeleteQuery = "DELTET FROM Student WHERE StudentID =" + id;
+                SqlCommand cmd = new SqlCommand(DeleteQuery, connection);
+                cmd.Parameters.AddWithValue("StudentID", id);
+                cmd.ExecuteNonQuery();
+                connection.Close();
+            }
+
+
+        }
+
+        public void DeleteModule(string Code)
+        {
+            using (SqlConnection connection = new SqlConnection(con))
+            {
+                string DeleteQuery = "DELTE FROM Student WHERE StudentID =" + Code;
+                SqlCommand cmd = new SqlCommand(DeleteQuery, connection);
+                cmd.Parameters.AddWithValue("ModuleCode", Code);
+                cmd.ExecuteNonQuery();
+                connection.Close();
+            }
+
+        }
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace MileStone2
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.Login = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAddUserName = new System.Windows.Forms.TextBox();
+            this.txtAddPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Login.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(104, 69);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword.TabIndex = 22;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(89, 155);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 20;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(32, 41);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(57, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "UserName";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(104, 41);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(100, 20);
+            this.txtUserName.TabIndex = 19;
+            // 
+            // Login
+            // 
+            this.Login.Controls.Add(this.btnLogin);
+            this.Login.Controls.Add(this.label2);
+            this.Login.Controls.Add(this.txtUserName);
+            this.Login.Controls.Add(this.txtPassword);
+            this.Login.Controls.Add(this.label5);
+            this.Login.Location = new System.Drawing.Point(143, 54);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(277, 258);
+            this.Login.TabIndex = 23;
+            this.Login.TabStop = false;
+            this.Login.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnAdd);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txtAddUserName);
+            this.groupBox2.Controls.Add(this.txtAddPassword);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(486, 54);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(277, 258);
+            this.groupBox2.TabIndex = 24;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Create New User";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(89, 155);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 20;
+            this.btnAdd.Text = "Add new user";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Password";
+            // 
+            // txtAddUserName
+            // 
+            this.txtAddUserName.Location = new System.Drawing.Point(104, 41);
+            this.txtAddUserName.Name = "txtAddUserName";
+            this.txtAddUserName.Size = new System.Drawing.Size(100, 20);
+            this.txtAddUserName.TabIndex = 19;
+            // 
+            // txtAddPassword
+            // 
+            this.txtAddPassword.Location = new System.Drawing.Point(104, 69);
+            this.txtAddPassword.Name = "txtAddPassword";
+            this.txtAddPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtAddPassword.TabIndex = 22;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 41);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "UserName";
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(890, 386);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.Login);
+            this.Name = "FrmLogin";
+            this.Text = "Login";
+            this.Login.ResumeLayout(false);
+            this.Login.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.GroupBox Login;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAddUserName;
+        private System.Windows.Forms.TextBox txtAddPassword;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
